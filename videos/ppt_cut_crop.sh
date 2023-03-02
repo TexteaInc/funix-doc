6420602e2f1646596ad98acb40da50a65cb0cad5
forrest <forrest.bao@gmail.com>
2023-03-02 00:35:45 -0600

    update PPT video

--- videos/ppt_cut_crop.sh
+++ videos/ppt_cut_crop.sh
@@ -1,1 +1,3 @@
-ffmpeg -i raw.mp4 -filter:v "crop=2510:1550:0:0" -ss 00:00:02 -t 00:00:50  out.mp4 
+# ffmpeg -i ppt_raw.mp4 -filter:v "crop=2510:1550:0:0" -ss 00:00:05 -t 00:00:50  ppt_out.mp4 
+
+ffmpeg -i ppt_out.mp4  -vf "fps=10,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" ppt_out.gif
